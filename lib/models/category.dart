6877ce06f1d36076b11f20dd268ffe028f8e2622
Waleed-Ibrahim14 <waleed.it13@gmail.com>
2022-06-27 17:19:25 +0200

    create dummy_categories file and add categories + add models/categores file.

---
+++ lib/models/category.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id;
+  final String title;
+  final color;
+
+  const Category({
+    required this.id,
+    required this.title,
+    this.color = Colors.orange, //?Here why we import material package.
+  });
+}
