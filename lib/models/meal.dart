4e66fb4cd97b337ac085427091f9b8a900b25f7d
Waleed-Ibrahim14 <waleed.it13@gmail.com>
2022-06-28 18:35:24 +0200

    edit meal class consturctor

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 enum Complexity {
   Simple,
-  Chalenging,
+  Challenging,
   Hard,
 }
 enum Affordability {
@@ -13,9 +13,9 @@ enum Affordability {
 
 class Meal {
   final String id;
-  final List<String> category;
+  final List<String> categories;
   final String title;
-  final String imgeUrl;
+  final String imageUrl;
   final List<String> ingredients;
   final List<String> steps;
   final int duration;
@@ -26,11 +26,11 @@ class Meal {
   final bool isVegan;
   final bool isVegetarian;
 
-  Meal({
+  const Meal({
     required this.id,
-    required this.category,
+    required this.categories,
     required this.title,
-    required this.imgeUrl,
+    required this.imageUrl,
     required this.ingredients,
     required this.steps,
     required this.duration,
